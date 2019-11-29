207e8242b08676d41a976e5632e9c40c46f88603
puliaev evgenii <puliaevevgenii@gmail.com>
2019-11-29 16:13:48 +0300

    Restructured project

--- src/com/company/utils/screen/Filter.java
+++
@@ -1,8 +0,0 @@
-package com.company.utils.screen;
-
-/**
- * Интерфейс декларирует метод, который будет отвечать, подходит ли заданный экземпляр класса какому-либо условию
- */
-public interface Filter<T> {
-    boolean permit(T value);
-}
--- src/com/company/utils/screen/colorizers/Colorizer.java
+++
@@ -1,7 +0,0 @@
-package com.company.utils.screen.colorizers;
-
-import java.awt.Color;
-
-public interface Colorizer {
-    Color getColor(float z, float dZ);
-}
